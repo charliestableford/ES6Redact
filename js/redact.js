0dc848667ba59d264f555bea8e54ff2e7a9430ff
Charlie Theodore <charliestableford2@gmail.com>
2020-10-15 20:50:08 -0700

    progress

--- js/redact.js
+++ js/redact.js
@@ -183,34 +183,45 @@ function isCollide(a, mouse) {
 let dataURL;
 document.querySelector(".download").addEventListener("click", function(e){
 	console.log(e);
-	console.log('in');
+	// console.log('in');
 	e.preventDefault();
 	dataURL = App.canvas.toDataURL('image/png');
+	localStorage.setItem("image",dataURL);
+	// document.write('<img src="'+dataURL+'"/>');
+	getLocal();
 });
 
-// $(".direct").click(function(){
-// 	uploadCanvas(dataURL);
-// });
-
-function uploadCanvas(dataURL){
-	console.log(dataURL);
-	// debugger;
-
-	async function saveQuote(){
-			try {
-			  const response = await fetch(endpoint)
-			  if (!response.ok) {
-				throw Error(response.statusText)
-			  }
-			  const data = await response.dataURL();
-			  console.log(data.dataURL);
-			} catch (err) {
-			  console.log(err)
-			  alert('Failed to save quote');
-			}
-	}
+function getLocal(){
+	let getInfo= localStorage.getItem("image",dataURL);
+	console.log(getInfo);
+	      // Opens a pop-up with twitter sharing dialog
+		  const shareURL = "http://twitter.com/share?"; //url base
+		  //params
+		  const params = {
+			url: "http://google.com", 
+			text: getLocal(),
+			via: "theRealDonaldTrump",
+			hashtags: "donald,blackOut"
+		  }
+		  for(var prop in params) 
+		  shareURL += '&' + prop + '=' + encodeURIComponent(params[prop]);
+		  window.open(shareURL, '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
 }
 
+// 	async function saveQuote(){
+// 			try {
+// 			  const response = await fetch(endpoint)
+// 			  if (!response.ok) {
+// 				throw Error(response.statusText)
+// 			  }
+// 			  const data = await response.dataURL();
+// 			  console.log(data.dataURL);
+// 			} catch (err) {
+// 			  console.log(err)
+// 			  alert('Failed to save quote');
+// 			}
+// 	}
+// }
 
 const App = {
     blackOutColor: "black",
@@ -248,24 +259,17 @@ const App = {
 		// 	}
 		//   }
 
-		  function displayQuote(quote) {
-			const quoteText = document.querySelector('.page');
-			quoteText.textContent = quote;
-			console.log(quote);
-		  }
+		//   function displayQuote(quote) {
+		// 	const quoteText = document.querySelector('.page');
+		// 	quoteText.textContent = quote;
+		// 	console.log(quote);
+		//   }
 
 			//   App.quote = getQuote();
             App.quote =  quoteArrOne[Math.floor(Math.random()*quoteArrOne.length)];
             App.blackOut();
             App.render();
             App.clear();       
-		});
-
-        document.querySelector(".quoteTwo").addEventListener("click", function(){      
-            App.quote =  quoteArrTwo[Math.floor(Math.random()*quoteArrTwo.length)];
-            App.blackOut();
-            App.render();   
-            App.clear();    
 		});
 
 	},
@@ -296,8 +300,6 @@ const App = {
 		writer = new Writer(ctx, 28);
 
 		quoteArrOne = ["China is doing very badly, worst year in 27 - was supposed to start buying our agricultural product now - no signs that they are doing so. That is the problem with China, they just don’t come through. Our Economy has become MUCH larger than the Chinese Economy is last 3 years....", "It snowed over 4 inches this past weekend in New York City. It is still October. So much for Global Warming.", "Newly released emails prove that scientists have manipulated data on global warming. The data is unreliable.", "In the 1920's people were worried about global cooling--it never happened. Now it's global warming. Give me a break!", "Tonight, we forcefully condemn the blatant corruption of the Democrat Party, the Fake News Media, and the rogue bureaucrats of the Deep State. The only message these radicals will understand is a crushing defeat on November 3, 2020!", "Despite the negative press, covefefe", "Crazy Joe Biden is trying to act like a tough guy. Actually, he is weak, both mentally and physically, and yet he threatens me, for the second time, with physical assault. He doesn't know me, but he would go down fast and hard, crying all the way. Don’t threaten people Joe!", "Why would Kim Jong-un insult me by calling me 'old,' when I would NEVER call him 'short and fat?' Oh well, I try so hard to be his friend - and maybe someday that will happen!", "The number of Coronavirus cases is strongly trending downward throughout the United States, with few exceptions. Very good news, indeed!"], "Was just informed that the Fake News from the Thursday White House Press Conference had me speaking & asking questions of Dr. Deborah Birx. Wrong, I was speaking to our Laboratory expert, not Deborah, about sunlight etc. & the CoronaVirus. The Lamestream Media is corrupt & sick!";
-
-		quoteArrTwo = ["If we are serious about climate change we can't just talk the talk, we've got to walk the walk and take on powerful special interests.", "Donald Trump believes climate change is a hoax. Donald Trump is an idoit.", "Abortion is a constitutional right", "I don't know if its a world record but Donald Trump has been sued more than 3,500 ties over the past 30 years.", "Thank you, I've had it, I'm going home, talk to you soon." , "Go vote or I'm sending everybody raisin cookes.", "In the United States it costs, on average, $12,000 to have a baby. In Finland it costs $60. We've got to end the disgrace of our profit-driven health care system and pass Medicare for all."];
 
 		String(quoteArrOne);
         // console.log(this.quote);
